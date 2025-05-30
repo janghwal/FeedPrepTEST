53e8df8d72e5d14e4fd8192fdfbaaa5ff322441f
janghwal <jhp337037@gmail.com>
2025-05-30 16:40:18 +0900

    feat: 기숙스택 엔티티에 유저 연관관계 매핑 추가
    - @JoinColumn을 사용하여 User 엔티티와 연동
    - @JoinColumn을 사용하여 tech_id 엔티티와 연동

--- src/main/java/com/example/feedprep/domain/techstack/entity/UserTechStack.java
+++ src/main/java/com/example/feedprep/domain/techstack/entity/UserTechStack.java
@@ -1,9 +1,11 @@
 package com.example.feedprep.domain.techstack.entity;
 
 import com.example.feedprep.common.entity.BaseTimeEntity;
+import com.example.feedprep.domain.user.entity.User;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -15,9 +17,11 @@ public class UserTechStack extends BaseTimeEntity {
 	@Id
 	private Long relationId;
 
-	// @ManyToOne
-	// private Users user;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 
 	@ManyToOne
+	@JoinColumn(name = "tech_id")
 	private TechStack techStack;
 }
