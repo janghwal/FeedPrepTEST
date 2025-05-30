7c269f3ef47f91a4bec86da2d3ab0a6cedcac587
janghwal <jhp337037@gmail.com>
2025-05-30 16:33:16 +0900

    feat: 구독 엔티티에 유저 연관관계 매핑 추가
    - @JoinColumn을 사용하여 User 엔티티와 연동

--- src/main/java/com/example/feedprep/domain/subscription/entity/Subscription.java
+++ src/main/java/com/example/feedprep/domain/subscription/entity/Subscription.java
@@ -1,11 +1,13 @@
 package com.example.feedprep.domain.subscription.entity;
 
 import com.example.feedprep.common.entity.BaseTimeEntity;
+import com.example.feedprep.domain.user.entity.User;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -18,9 +20,11 @@ public class Subscriptions extends BaseTimeEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long subscriptionId;
 
-	// @ManyToOne
-	// private Users sender;
-	// @ManyToOne
-	// private Users receiver;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User sender;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User receiver;
 
 }
