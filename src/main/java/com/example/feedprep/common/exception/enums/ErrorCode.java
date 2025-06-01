e2c63f19faaed74ea6bc3f791ee9e5ef77e03a5c
janghwal <jhp337037@gmail.com>
2025-06-01 15:12:25 +0900

    feat: 구독 모듈에서 사용 할 ErrorCode 추가
    - 유저 모듈의 에러코드 임시 정의하여 사용

--- src/main/java/com/example/feedprep/common/exception/enums/ErrorCode.java
+++ src/main/java/com/example/feedprep/common/exception/enums/ErrorCode.java
@@ -11,6 +11,10 @@ public enum ErrorCode {
     MISSING_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다."),
     INVALID_JWT_SIGNATURE(HttpStatus.UNAUTHORIZED, "유효하지 않은 JWT 서명입니다."),
 
+    // 구독
+    CANNOT_SUBSCRIBE_SELF(HttpStatus.BAD_REQUEST, "자기 자신을 구독할 수 없습니다."),
+    // 유저 모듈 임시 에러 코드
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
 
     ;
 
