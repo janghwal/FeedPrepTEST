0bd1b64ccdbd21ba76754f0b2a5ea70669b68c02
janghwal <jhp337037@gmail.com>
2025-06-01 15:14:33 +0900

    feat: 구독하기 기능 컨트롤러 메서드 구현

--- src/main/java/com/example/feedprep/domain/subscription/controller/SubscriptionController.java
+++ src/main/java/com/example/feedprep/domain/subscription/controller/SubscriptionController.java
@@ -1,9 +1,34 @@
 package com.example.feedprep.domain.subscription.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.feedprep.common.exception.enums.SuccessCode;
+import com.example.feedprep.common.response.ApiResponseDto;
+import com.example.feedprep.domain.subscription.service.SubscriptionService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
-@RequestMapping()
+@RequestMapping("/subscriptions")
+@RequiredArgsConstructor
 public class SubscriptionController {
+	private final SubscriptionService subscriptionService;
+
+	@PostMapping
+	public ResponseEntity<ApiResponseDto<Void>> subscribe(
+		// @AuthenticationPrincipal CustomUserPrincipal customUserPrincipal,
+		@RequestParam Long userId
+	) {
+		// 임시 senderId
+		Long senderId = 1L;
+
+		subscriptionService.subscribe(senderId, userId);
+
+		return ResponseEntity.status(SuccessCode.SUBSCRIBED.getHttpStatus())
+			.body(ApiResponseDto.success(SuccessCode.SUBSCRIBED));
+	}
 }
