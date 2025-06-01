a2ec0835405e6ef1a762103af4feea0279416395
janghwal <jhp337037@gmail.com>
2025-06-01 13:14:44 +0900

    feat: 구독 모듈에서 사용 할 SuccessCode 추가

--- src/main/java/com/example/feedprep/common/exception/enums/SuccessCode.java
+++ src/main/java/com/example/feedprep/common/exception/enums/SuccessCode.java
@@ -11,7 +11,11 @@ public enum SuccessCode {
     // 회원가입 및 로그인 성공
     LOGIN_SUCCESS(HttpStatus.OK, "로그인에 성공하였습니다."),
 
-
+    // 구독 모듈
+    SUBSCRIBED(HttpStatus.CREATED, "구독하였습니다."),
+    UNSUBSCRIBED(HttpStatus.OK, "구독을 취소하였습니다."),
+    SUBSCRIPTION_LIST(HttpStatus.OK, "구독 목록입니다."),
+    SUBSCRIBER_LIST(HttpStatus.OK, "구독자 목록입니다."),
 
 
     ;
