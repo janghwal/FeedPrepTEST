e9771e5741d16146216946bd623f4038f092663e
janghwal <jhp337037@gmail.com>
2025-06-01 15:14:41 +0900

    feat: 구독하기 기능 서비스 메서드 구현

--- src/main/java/com/example/feedprep/domain/subscription/service/SubscriptionService.java
+++ src/main/java/com/example/feedprep/domain/subscription/service/SubscriptionService.java
@@ -1,4 +1,5 @@
 package com.example.feedprep.domain.subscription.service;
 
 public interface SubscriptionService {
+	void subscribe(Long senderId, Long userId);
 }
--- src/main/java/com/example/feedprep/domain/subscription/service/SubscriptionServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/subscription/service/SubscriptionServiceImpl.java
@@ -1,7 +1,46 @@
 package com.example.feedprep.domain.subscription.service;
 
+import java.util.Objects;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.example.feedprep.common.exception.base.CustomException;
+import com.example.feedprep.common.exception.enums.ErrorCode;
+import com.example.feedprep.domain.subscription.entity.Subscription;
+import com.example.feedprep.domain.subscription.repository.SubscriptionRepository;
+import com.example.feedprep.domain.user.entity.User;
+import com.example.feedprep.domain.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class SubscriptionServiceImpl implements SubscriptionService{
+
+	private final UserRepository userRepository;
+	private final SubscriptionRepository subscriptionRepository;
+
+	@Override
+	public void subscribe(Long senderId, Long userId) {
+		// 자기 자신 구독 요청
+		if(Objects.equals(senderId, userId)){
+			throw new CustomException(ErrorCode.CANNOT_SUBSCRIBE_SELF);
+		}
+
+		// UserRepository findByIdOrElseThrow 정의되면 수정
+		Optional<User> sender = userRepository.findById(senderId);
+		if(sender.isEmpty()){
+			throw new CustomException(ErrorCode.USER_NOT_FOUND);
+		}
+		Optional<User> receiver = userRepository.findById(userId);
+		if(receiver.isEmpty()){
+			throw new CustomException(ErrorCode.USER_NOT_FOUND);
+		}
+
+		// 구독은 튜터에게만 가능한 것인가? 의사 결정 후 추가 필요.
+
+		Subscription subscription = new Subscription(sender.get(), receiver.get());
+		subscriptionRepository.save(subscription);
+	}
 }
