5db3088324c2f00ceb70b02f37b83d841c64530a
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-02 11:17:01 +0900

    feat: 피드백 요청 로직업데이트 fix: feedbackId 파라미터 추가

--- src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackServiceImpl.java
@@ -49,16 +49,21 @@ public class FeedbackServiceImpl implements FeedbackService{
 		return new FeedbackRequestEntityResponseDto(getInfoRequest);
 	}
 
+	@Transactional(readOnly = true)
 	@Override
-	public FeedbackRequestEntityResponseDto getRequest(Long tutorId, Long documentId, Long page, Long size,
+	public FeedbackRequestEntityResponseDto getRequest(Long userId,Long tutorId, Long documentId, Long page, Long size,
 		LocalDateTime time) {
 		//유저 본인 확인
+		User user =  userRepository.findById(userId).orElseThrow(()-> new CustomException(ErrorCode.INVALID_TUTOR));
+		if(!user.getUserId().equals(userId)){
+			//예외 반환
+		}
 		//조건에 맞게 조회 출력후 무조건 DESC로 할것.
 		return null;
 	}
 
 	@Override
-	public FeedbackRequestEntityResponseDto updateRequest(FeedbackRequestDto dto, Long userId) {
+	public FeedbackRequestEntityResponseDto updateRequest(FeedbackRequestDto dto,Long feedbackId, Long userId) {
 		//유저 본인 확인
 		//요청이 존재하는 가?
 		//수정 적용
