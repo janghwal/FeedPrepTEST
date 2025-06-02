7ab4af391727c0a792e9128eb086d939bf18f641
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-02 10:42:20 +0900

    feat: user, document 등의 Repository 의존성 추가

--- src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackServiceImpl.java
@@ -2,12 +2,31 @@ package com.example.feedprep.domain.feedbackrequestentity.service;
 
 import java.time.LocalDateTime;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.feedprep.common.exception.base.CustomException;
 import com.example.feedprep.common.response.ApiResponseDto;
+import com.example.feedprep.domain.document.entity.Document;
+import com.example.feedprep.domain.document.repository.DocumentRepository;
+import com.example.feedprep.domain.feedback.repository.FeedBackRepository;
 import com.example.feedprep.domain.feedbackrequestentity.dto.request.FeedbackRequestDto;
 import com.example.feedprep.domain.feedbackrequestentity.dto.response.FeedbackRequestEntityResponseDto;
+import com.example.feedprep.domain.feedbackrequestentity.entity.FeedbackRequestEntity;
+import com.example.feedprep.domain.user.entity.User;
+import com.example.feedprep.domain.user.repository.UserRepository;
 
+@Service
+@Repository
+@RequiredArgsConstructor
 public class FeedbackServiceImpl implements FeedbackService{
+	private FeedBackRepository feedBackRepository;
+	private UserRepository userRepository;
+	private DocumentRepository documentRepository;
+
+	@Transactional
 	@Override
 	public FeedbackRequestEntityResponseDto saveRequest(FeedbackRequestDto dto, Long userId) {
 
