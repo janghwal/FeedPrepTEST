8e4f61ae3034c8acbf0ff9bbdb8d24847da466df
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-02 12:28:33 +0900

    feat: 유저 와 튜터 동시 조회문 추가

--- src/main/java/com/example/feedprep/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/feedprep/domain/user/repository/UserRepository.java
@@ -1,9 +1,22 @@
 package com.example.feedprep.domain.user.repository;
 
+import java.util.List;
+import java.util.Map;
+
+import com.example.feedprep.common.exception.base.CustomException;
 import com.example.feedprep.domain.user.entity.User;
+import com.example.feedprep.domain.user.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	//피드백 요청용 조회 문
+
+	@Query("SELECT u FROM User u WHERE u.userId IN(:userId,:tutorId)")
+	List<User> findByIds(@Param("userId") Long userId, @Param("tutorId") Long tutorId);
+
 }
