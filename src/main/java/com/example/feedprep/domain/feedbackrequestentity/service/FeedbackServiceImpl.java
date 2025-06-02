5c4f54991596744e2d55a56ff49224c2cbea9a82
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-02 11:21:46 +0900

    feat: 피드백 요청 수정 로직 추가.

--- src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackServiceImpl.java
@@ -65,7 +65,16 @@ public class FeedbackServiceImpl implements FeedbackService{
 	@Override
 	public FeedbackRequestEntityResponseDto updateRequest(FeedbackRequestDto dto,Long feedbackId, Long userId) {
 		//유저 본인 확인
+		User user =  userRepository.findById(dto.getTutorId()).orElseThrow(()-> new CustomException(ErrorCode.INVALID_TUTOR));
+		if(!user.getUserId().equals(userId)){
+			//예외 반환
+		}
 		//요청이 존재하는 가?
+		FeedbackRequestEntity request = feedBackRepository.findById(feedbackId).orElseThrow(()->new CustomException(ErrorCode.FEEDBACK_NOT_FOUND));
+		if(request.getUser().getUserId().equals(userId))
+		{
+
+		}
 		//수정 적용
 		return null;
 	}
