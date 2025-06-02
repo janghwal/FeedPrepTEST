b107ea64c7f7b5ee177cd3c5e8378612da1c5e3f
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-02 09:54:26 +0900

    feat: 피드백 요청 DTO 추가

---
+++ src/main/java/com/example/feedprep/domain/feedbackrequestentity/dto/request/FeedbackRequestDTO.java
@@ -0,0 +1,10 @@
+package com.example.feedprep.domain.feedbackrequestentity.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class FeedbackRequestDTO {
+	private Long tutorId;
+	private Long documentId;
+	private String content;
+}
