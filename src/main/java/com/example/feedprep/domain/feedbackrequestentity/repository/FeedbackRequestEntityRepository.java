baf28200084cd1dcc9b8b38726b934628410c0c9
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-03 15:09:55 +0900

    feat: 중복 신청 방지 조회 기능 추가

--- src/main/java/com/example/feedprep/domain/feedbackrequestentity/repository/FeedbackRequestEntityRepository.java
+++ src/main/java/com/example/feedprep/domain/feedbackrequestentity/repository/FeedbackRequestEntityRepository.java
@@ -1,11 +1,25 @@
 package com.example.feedprep.domain.feedbackrequestentity.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import com.example.feedprep.domain.feedbackrequestentity.common.RequestState;
 import com.example.feedprep.domain.feedbackrequestentity.entity.FeedbackRequestEntity;
 
 @Repository
 public interface FeedbackRequestEntityRepository extends JpaRepository<FeedbackRequestEntity, Long>
 	, FeedbackRequestEntityRepositoryCustom{
 
+	@Query("SELECT f FROM FeedbackRequestEntity f "
+		+ "WHERE f.user.userId = :userId "
+		+ "AND f.tutor.userId = :tutorId "
+		+ "AND f.requestState = :state"
+	)
+	Optional<FeedbackRequestEntity> findTop1ByUser_UserIdAndTutor_UserIdAndContentAndRequestState(
+		@Param("userId")Long userId
+		,@Param("tutorId")Long tutorId
+		,@Param("state") RequestState state);
 }
