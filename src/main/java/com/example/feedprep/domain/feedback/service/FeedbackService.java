c76d351c4517db86a89db2eff58a2d6841fd9045
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-04 17:53:58 +0900

    feat:서비스 기능 이름 주석 처리및 누락 파라미터 보충

--- src/main/java/com/example/feedprep/domain/feedback/service/FeedbackService.java
+++ src/main/java/com/example/feedprep/domain/feedback/service/FeedbackService.java
@@ -2,12 +2,21 @@ package com.example.feedprep.domain.feedback.service;
 
 import com.example.feedprep.common.response.ApiResponseDto;
 import com.example.feedprep.domain.feedback.dto.request.FeedbackRequestDto;
+import com.example.feedprep.domain.feedback.dto.response.FeedbackRequestListResponseDto;
+import com.example.feedprep.domain.feedback.dto.response.FeedbackRequestResponseDto;
 import com.example.feedprep.domain.feedback.dto.response.FeedbackResponse;
 
 public interface FeedbackService {
 
+  //요청 리스트 조회 단건
+   FeedbackRequestResponseDto getFeedbackRequest(Long userId, Long requestId);
+  //요청 리스트 조회 다건
+   FeedbackRequestListResponseDto getFeedbackRequestList(Long userId , int page, int size);
+  //요청 리스트 생성
   FeedbackResponse createFeedback(Long userId, Long requestId, FeedbackRequestDto dto);
+  //요청 리스트 수정
   FeedbackResponse updateFeedback(Long userId, Long requestId, FeedbackRequestDto dto);
+  //요청 리스트 거절
   ApiResponseDto rejectFeedback(Long userId, Long requestId,  FeedbackRequestDto dto);
 
 }
