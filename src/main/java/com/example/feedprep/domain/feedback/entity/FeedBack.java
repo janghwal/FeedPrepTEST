b034cb7782b1cb3382a0e9e25656a92e1914ec19
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-04 18:00:42 +0900

    feat: 피드백 엔티티 업데이트

--- src/main/java/com/example/feedprep/domain/feedback/entity/FeedBack.java
+++ src/main/java/com/example/feedprep/domain/feedback/entity/FeedBack.java
@@ -2,10 +2,12 @@ package com.example.feedprep.domain.feedback.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -14,13 +16,17 @@ import lombok.RequiredArgsConstructor;
 
 import com.example.feedprep.common.entity.BaseTimeEntity;
 import com.example.feedprep.domain.feedback.common.RejectReason;
+import com.example.feedprep.domain.feedback.dto.request.FeedbackRequestDto;
 import com.example.feedprep.domain.feedbackrequestentity.entity.FeedbackRequestEntity;
+import com.example.feedprep.domain.user.entity.User;
 
 @Getter
 @Entity
 @Table(name = "feedbacks")
 @RequiredArgsConstructor
-public class FeedBack extends BaseTimeEntity {
+public class Feedback extends BaseTimeEntity{
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -29,15 +35,30 @@ public class FeedBack extends BaseTimeEntity {
 	@JoinColumn(name="request_id")
 	private FeedbackRequestEntity feedbackRequestEntity;
 
-    @Column(nullable = false)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "tutor_id", nullable = false)
+	private User tutor;
+
 	private String content;
 
 	private RejectReason rejectReason;
 
 	private String etcContent;
 
-	public FeedBack(FeedbackRequestEntity feedBackRequest, String content) {
-		this.feedbackRequestEntity = feedBackRequest;
-		this.content = content;
+	public Feedback(FeedbackRequestDto requestDto, User tutor){
+		this.content = requestDto.getContent();
+		this.rejectReason = requestDto.getRejectReason();
+		this.etcContent = requestDto.getEtcContent();
+		this.tutor =tutor;
+	}
+
+	public void updateFeedback(FeedbackRequestDto requestDto){
+		this.content = requestDto.getContent();
+		this.rejectReason = requestDto.getRejectReason();
+		this.etcContent = requestDto.getEtcContent();
+	}
+
+	public void updateFeedbackRequest(FeedbackRequestEntity entity){
+		this.feedbackRequestEntity = entity;
 	}
 }
