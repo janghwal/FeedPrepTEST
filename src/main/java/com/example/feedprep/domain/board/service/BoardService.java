2c9b877965b6b589498bcd44e19d6a83d3d74a14
yigani <144660094+yigani@users.noreply.github.com>
2025-06-04 12:50:28 +0900

    feat(게시판 CRUD)
    .

--- src/main/java/com/example/feedprep/domain/board/service/BoardService.java
+++ src/main/java/com/example/feedprep/domain/board/service/BoardService.java
@@ -1,4 +1,24 @@
 package com.example.feedprep.domain.board.service;
 
-public class BoardService {
+import com.example.feedprep.domain.board.dto.BoardRequestDto;
+import com.example.feedprep.domain.board.dto.BoardResponseDto;
+import com.example.feedprep.domain.board.dto.BoardSearchCondition;
+
+import java.util.List;
+
+public interface BoardService {
+
+    BoardResponseDto createBoard(BoardRequestDto requestDto);
+
+    List<BoardResponseDto> getBoards(BoardSearchCondition condition);
+
+    BoardResponseDto getBoard(Long boardId);
+
+    void updateBoard(Long boardId, BoardRequestDto requestDto);
+
+    void deleteBoard(Long boardId);
+
+    int recommendBoard(Long boardId);
+
+    boolean scrapBoard(Long boardId);
 }
