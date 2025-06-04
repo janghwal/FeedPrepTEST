9891b0706611bb3bbb98a64aa716491f497c57e0
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-04 16:48:09 +0900

    feat: Feedback 수정 서비스 업데이트

--- src/main/java/com/example/feedprep/domain/feedback/service/FeedbackServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/feedback/service/FeedbackServiceImpl.java
@@ -36,7 +36,7 @@ public class FeedbackServiceImpl implements FeedbackService{
 			throw new RuntimeException("현재 요청이 취소 되었거나 완료가되었습니다.");
 		}
 		// 4. Feedback 엔티티 생성
-		Feedback feedback = new Feedback(dto);
+		Feedback feedback = new Feedback(dto, tutor);
 		request.updateRequestState(RequestState.COMPLETED);
 		feedback.updateFeedbackRequest(request);
 		// 6. 저장 (트랜잭션 내에서)
@@ -47,10 +47,24 @@ public class FeedbackServiceImpl implements FeedbackService{
 		return new FeedbackResponse(saveFeedback);
 	}
 
-
-	public FeedbackResponse updateFeedback(Long tutorId, Long feedbackId, FeedbackRequestDto dto) {
-
-	return  null;
+	@Transactional
+	public FeedbackResponse updateFeedback(Long userId, Long feedbackId, FeedbackRequestDto dto) {
+		// 1. 튜터 본인 확인
+		User tutor = userRepository.findByIdOrElseThrow(userId);
+		// 2. 피드백 존재 여부 확인
+		Feedback feedback = feedBackRepository.findById(feedbackId).orElseThrow(()->new RuntimeException("없습니다."));
+		// 3. 본인 피드백인지 검사
+		if(feedback.getTutor().equals(tutor)){
+			throw new RuntimeException("너님이 만든게 아닌거 알아 나가!");
+		}
+		// 4. dto 내용으로 feedback 업데이트
+		feedback.updateFeedback(dto);
+		// 5. 저장 (save)
+		Feedback saveFeedback = feedBackRepository.save(feedback);
+		// 6. 알림/이벤트 (선택)
+		// 7. 알림 or 이벤트 발행 (선택)
+		// 8. Response 반환
+		return new FeedbackResponse(saveFeedback);
 	}
 
 	public ApiResponseDto rejectFeedback(Long userId, Long requestId) {
