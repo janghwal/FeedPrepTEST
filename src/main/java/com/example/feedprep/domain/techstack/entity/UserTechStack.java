8b7faee7383397d045b604c4791241bb587038ca
janghwal <jhp337037@gmail.com>
2025-06-04 12:32:14 +0900

    refactor: TechStack-UserTechStack 연관관계 LAZY 로딩으로 변경

--- src/main/java/com/example/feedprep/domain/techstack/entity/UserTechStack.java
+++ src/main/java/com/example/feedprep/domain/techstack/entity/UserTechStack.java
@@ -4,6 +4,7 @@ import com.example.feedprep.common.entity.BaseTimeEntity;
 import com.example.feedprep.domain.user.entity.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,7 +27,7 @@ public class UserTechStack extends BaseTimeEntity {
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "tech_id")
 	private TechStack techStack;
 
