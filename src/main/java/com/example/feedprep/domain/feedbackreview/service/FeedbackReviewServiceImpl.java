f3ebd74b9cdd5dcea68fbcb0bf18037f6788254e
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-05 12:05:35 +0900

    feat: 리뷰 생성 추가

--- src/main/java/com/example/feedprep/domain/feedbackreview/service/FeedbackReviewServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/feedbackreview/service/FeedbackReviewServiceImpl.java
@@ -3,11 +3,14 @@ package com.example.feedprep.domain.feedbackreview.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+import com.example.feedprep.common.exception.base.CustomException;
+import com.example.feedprep.common.exception.enums.ErrorCode;
 import com.example.feedprep.common.response.ApiResponseDto;
 import com.example.feedprep.domain.feedback.entity.Feedback;
 import com.example.feedprep.domain.feedback.repository.FeedBackRepository;
 import com.example.feedprep.domain.feedbackreview.dto.FeedbackReviewRequestDto;
 import com.example.feedprep.domain.feedbackreview.dto.FeedbackReviewResponseDto;
+import com.example.feedprep.domain.feedbackreview.entity.FeedbackReview;
 import com.example.feedprep.domain.feedbackreview.repository.FeedBackReviewRepository;
 import com.example.feedprep.domain.user.entity.User;
 import com.example.feedprep.domain.user.repository.UserRepository;
@@ -20,7 +23,17 @@ public class FeedbackReviewServiceImpl implements FeedbackReviewService {
     private final UserRepository userRepository;
 	@Override
 	public FeedbackReviewResponseDto saveReview(FeedbackReviewRequestDto dto, Long userId, Long feedbackId) {
-	    return null;
+		User user = userRepository.findByIdOrElseThrow(userId);
+		if(!user.getUserId().equals(userId)){
+			throw new CustomException(ErrorCode.UNAUTHORIZED_REQUESTER_ACCESS);
+		}
+		Feedback feedback = feedBackRepository.findById(feedbackId)
+			.orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_FEEDBACK));
+
+		FeedbackReview feedbackReview = new FeedbackReview(dto, feedback, user);
+		FeedbackReview saveReview = feedBackReviewRepository.save(feedbackReview);
+
+	    return new FeedbackReviewResponseDto(saveReview);
 	}
 
 	@Override
