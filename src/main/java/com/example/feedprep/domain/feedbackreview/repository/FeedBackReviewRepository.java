cfb9dea23571f1c031c8a307d11dc74f89a1f364
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-05 11:38:24 +0900

    feat: 피드백 리뷰 명칭 수정

--- src/main/java/com/example/feedprep/domain/feedbackreview/repository/FeedBackReviewRepository.java
+++ src/main/java/com/example/feedprep/domain/feedbackreview/repository/FeedBackReviewRepository.java
@@ -2,8 +2,8 @@ package com.example.feedprep.domain.feedbackreview.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.feedprep.domain.feedbackreview.entity.FeedBackReview;
+import com.example.feedprep.domain.feedbackreview.entity.FeedbackReview;
 
 @Repository
-public interface FeedBackReviewRepository extends JpaRepository<FeedBackReview, Long> {
+public interface FeedBackReviewRepository extends JpaRepository<FeedbackReview, Long> {
 }
