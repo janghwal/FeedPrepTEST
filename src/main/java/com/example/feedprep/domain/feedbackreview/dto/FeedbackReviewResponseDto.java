693305f7ba9551ff5372bd4fb0b442dd86c6fd40
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-05 11:39:24 +0900

    feat: 피드백 리뷰 응답 Dto 추가

---
+++ src/main/java/com/example/feedprep/domain/feedbackreview/dto/FeedbackReviewResponseDto.java
@@ -0,0 +1,29 @@
+package com.example.feedprep.domain.feedbackreview.dto;
+
+import lombok.Getter;
+
+import com.example.feedprep.domain.feedback.entity.Feedback;
+import com.example.feedprep.domain.feedbackreview.entity.FeedbackReview;
+import com.example.feedprep.domain.user.entity.User;
+
+@Getter
+public class FeedbackReviewResponseDto {
+
+	private Long id;
+
+	private Long userId;
+
+	private Long tutorId;
+
+	private int Rating;
+
+	private  String Content;
+
+	public FeedbackReviewResponseDto(FeedbackReview feedbackReview, User user) {
+		this.id = feedbackReview.getId();
+		this.userId = user.getUserId();
+		this.tutorId =feedbackReview.getTutorId();
+		this.Rating = feedbackReview.getRating();
+		this.Content = feedbackReview.getContent();
+	}
+}
