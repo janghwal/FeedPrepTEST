332b81312f4b1c3a2bf51556a334c5bfe45e3c76
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-05 14:36:33 +0900

    feat: 리뷰 목록 다건 조회문 추가

--- src/main/java/com/example/feedprep/domain/feedbackreview/repository/FeedBackReviewRepository.java
+++ src/main/java/com/example/feedprep/domain/feedbackreview/repository/FeedBackReviewRepository.java
@@ -1,9 +1,21 @@
 package com.example.feedprep.domain.feedbackreview.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.feedprep.domain.feedbackreview.entity.FeedbackReview;
 
 @Repository
 public interface FeedBackReviewRepository extends JpaRepository<FeedbackReview, Long> {
+
+	//학생이 작성한 리뷰를 확인
+	@Query("SELECT fr FROM FeedbackReview fr WHERE fr.userId = :userId AND fr.deletedAt IS null ")
+	Page<FeedbackReview> findByUserIdAndDeletedAtIsNull(@Param("userId")Long userId, PageRequest pageable);
+
+	//튜터에게 작성한 학생들의 리뷰를 조회
+	@Query("SELECT fr FROM FeedbackReview fr WHERE fr.tutorId = :tutorId AND fr.deletedAt IS null ")
+	Page<FeedbackReview> findByTutorIdAndDeletedAtIsNull(@Param("tutorId")Long tutorId, PageRequest pageable);
 }
