28c5580068ac95b6c055e19be1b820c17c940203
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-05 14:36:00 +0900

    feat: 리뷰 목록 다건 조회 기능 추가

--- src/main/java/com/example/feedprep/domain/feedbackreview/service/FeedbackReviewService.java
+++ src/main/java/com/example/feedprep/domain/feedbackreview/service/FeedbackReviewService.java
@@ -1,5 +1,7 @@
 package com.example.feedprep.domain.feedbackreview.service;
 
+import java.util.List;
+
 import com.example.feedprep.common.response.ApiResponseDto;
 import com.example.feedprep.domain.feedbackreview.dto.FeedbackReviewRequestDto;
 import com.example.feedprep.domain.feedbackreview.dto.FeedbackReviewResponseDto;
@@ -9,6 +11,9 @@ public interface FeedbackReviewService {
 
 	FeedbackReviewResponseDto saveReview(FeedbackReviewRequestDto dto, Long userId, Long feedbackId);
 	FeedbackReviewResponseDto getReview(Long userId, Long reviewId);
+	List<FeedbackReviewResponseDto> getWrittenReviewsByStudent(Long userId, Long reviewId, int page, int size);
+	List<FeedbackReviewResponseDto> getReceivedReviewsForTutor(Long userId, Long reviewId, int page, int size);
+	Float getAverageRating(Long tutorId);
 	FeedbackReviewResponseDto updateReview(FeedbackReviewRequestDto dto, Long userId, Long reviewId);
 	ApiResponseDto deleteReview(Long userId, Long feedbackId, Long reviewId);
 }
