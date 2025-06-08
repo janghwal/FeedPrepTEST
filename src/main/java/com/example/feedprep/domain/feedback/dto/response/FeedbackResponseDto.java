17034e5ba16b82f049451ad9e27615f4c6a64ae1
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-08 18:13:12 +0900

    feet:피드백 응답 수정 최소한의 정보 출력으로 수정

--- src/main/java/com/example/feedprep/domain/feedback/dto/response/FeedbackResponseDto.java
+++ src/main/java/com/example/feedprep/domain/feedback/dto/response/FeedbackResponseDto.java
@@ -7,13 +7,16 @@ import com.example.feedprep.domain.feedback.entity.Feedback;
 public class FeedbackResponseDto {
 
 	private Long id;
+	private Long studentId;
 	private Long feedbackRequestEntityId;
 	private String content;
+	private String modifiedAt;
 
 	public FeedbackResponseDto(Feedback feedBack){
 		this.id = feedBack.getId();
+		this.studentId = feedBack.getFeedbackRequestEntity().getUser().getUserId();
 		this.feedbackRequestEntityId = feedBack.getFeedbackRequestEntity().getId();
 		this.content = feedBack.getContent();
-
+		this.modifiedAt = feedBack.getModifiedAt().toString();
 	}
 }
