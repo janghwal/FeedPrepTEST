f6721a0e3e0378454ad9cfbb470e5047a1bca2d2
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-08 18:15:06 +0900

    feet:피드백 요청 응답 출력 내용 추가

--- src/main/java/com/example/feedprep/domain/feedback/dto/response/FeedbackRequestResponseDto.java
+++ src/main/java/com/example/feedprep/domain/feedback/dto/response/FeedbackRequestResponseDto.java
@@ -1,21 +1,36 @@
 package com.example.feedprep.domain.feedback.dto.response;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import com.example.feedprep.domain.feedbackrequestentity.common.RequestState;
 import com.example.feedprep.domain.feedbackrequestentity.entity.FeedbackRequestEntity;
 import com.example.feedprep.domain.user.entity.User;
 
 @Getter
 @RequiredArgsConstructor
 public class FeedbackRequestResponseDto {
+
+	private Long requestId;
 	private Long tutorId;
-	private String nickname;
-	private FeedbackRequestEntity requests;
+	private Long studentId;
+	private Long documentId;
+	private String fileUrl;
+	private String content;
+	private RequestState requestState;
+	private String ModifiedAt;
 
 	public FeedbackRequestResponseDto(User user, FeedbackRequestEntity requests) {
-		this.tutorId = user.getUserId();
-		this.nickname = user.getName();
-		this.requests = requests;
+		this.requestId = requests.getId();
+		this.tutorId = requests.getTutor().getUserId();
+		this.studentId = requests.getUser().getUserId();
+		this.documentId = requests.getDocument().getDocumentId();
+		this.fileUrl =  requests.getDocument().getFileUrl();
+		this.content =  requests.getContent();
+		this.requestState = requests.getRequestState();
+		this.ModifiedAt = requests.getModifiedAt().toString();
 	}
+
 }
