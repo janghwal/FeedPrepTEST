2f716e3fbfb9dc65b42a54f6b48c2cc75bffb2f1
janghwal <jhp337037@gmail.com>
2025-06-09 11:33:25 +0900

    feat: 튜터 매칭 Dto 추가

---
+++ src/main/java/com/example/feedprep/domain/matching/dto/MatchingDto.java
@@ -0,0 +1,12 @@
+package com.example.feedprep.domain.matching.dto;
+
+import java.util.List;
+
+public class MatchingDto {
+	private Long tutorId;
+	private String name;
+	private Double rating;
+	private String introduction;
+	private List<String> techStacks;
+
+}
