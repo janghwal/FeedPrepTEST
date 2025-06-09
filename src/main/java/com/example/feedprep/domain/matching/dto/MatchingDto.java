aaa39368cf94b655207315614328f1d69dc483f6
janghwal <jhp337037@gmail.com>
2025-06-09 13:13:45 +0900

    feat: MatchingDto 생성자 추가

--- src/main/java/com/example/feedprep/domain/matching/dto/MatchingDto.java
+++ src/main/java/com/example/feedprep/domain/matching/dto/MatchingDto.java
@@ -2,11 +2,24 @@ package com.example.feedprep.domain.matching.dto;
 
 import java.util.List;
 
+import com.querydsl.core.Tuple;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public class MatchingDto {
 	private Long tutorId;
 	private String name;
 	private Double rating;
 	private String introduction;
+	@Setter
 	private List<String> techStacks;
 
+	public MatchingDto(Tuple tuple){
+		this.tutorId = tuple.get(0, Long.class);
+		this.name = tuple.get(1, String.class);
+		this.rating = tuple.get(2, Double.class);
+		this.introduction = tuple.get(3, String.class);
+	}
 }
