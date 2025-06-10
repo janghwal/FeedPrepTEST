71f17423536560a32c3160049b6c9ae73bdff1f2
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-10 09:09:05 +0900

    feat: 피드백 요청 일부 기능 삭제

--- src/main/java/com/example/feedprep/domain/feedback/controller/FeedbackController.java
+++ src/main/java/com/example/feedprep/domain/feedback/controller/FeedbackController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.feedprep.common.response.ApiResponseDto;
 import com.example.feedprep.common.security.annotation.AuthUser;
-import com.example.feedprep.domain.feedback.dto.request.FeedbackRejectRequestDto;
 import com.example.feedprep.domain.feedback.dto.request.FeedbackWriteRequestDto;
-import com.example.feedprep.domain.feedback.dto.response.FeedbackRequestListResponseDto;
-import com.example.feedprep.domain.feedback.dto.response.FeedbackRequestResponseDto;
 import com.example.feedprep.domain.feedback.dto.response.FeedbackResponseDto;
 import com.example.feedprep.domain.feedback.service.FeedbackService;
 
@@ -27,30 +22,6 @@ import com.example.feedprep.domain.feedback.service.FeedbackService;
 @RequiredArgsConstructor
 public class FeedbackController {
 	private final FeedbackService feedbackService;
-
-	@GetMapping("/feedback/requests/{requestId}")
-	public ResponseEntity<FeedbackRequestResponseDto>  getFeedbackRequest(
-		@AuthUser Long tutorId,
-		@PathVariable  Long requestId
-	){
-		return  new ResponseEntity<>(feedbackService.getFeedbackRequest(tutorId, requestId), HttpStatus.OK);
-	}
-	@GetMapping("/feedback/requests/{requestId}")
-	public ResponseEntity<FeedbackRequestListResponseDto> getFeedbackRequests(
-		@AuthUser Long tutorId,
-		@RequestParam(defaultValue = "0") int page,
-		@RequestParam(defaultValue = "20") int size
-	){
-		return new ResponseEntity<>(feedbackService.getFeedbackRequests(tutorId, page, size), HttpStatus.OK);
-	}
-
-	@PatchMapping ("/feedback/requests/{requestId}/reject")
-	public ResponseEntity<ApiResponseDto> rejectFeedbackRequest(
-		@AuthUser Long tutorId,
-		@PathVariable Long requestId,
-		@Validated @RequestBody FeedbackRejectRequestDto dto){
-		return  new ResponseEntity<>(feedbackService.rejectFeedbackRequest(tutorId, requestId, dto), HttpStatus.OK);
-	}
 
 	@PostMapping("/feedback")
 	public ResponseEntity<FeedbackResponseDto> createFeedback(
