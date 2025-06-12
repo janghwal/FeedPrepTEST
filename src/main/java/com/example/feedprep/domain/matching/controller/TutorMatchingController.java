d4883650700f436611cec6c8c35822faae0abf46
janghwal <jhp337037@gmail.com>
2025-06-12 19:38:15 +0900

    fix: 잘못된 컨트롤러 URL 매핑 수정

--- src/main/java/com/example/feedprep/domain/matching/controller/TutorMatchingController.java
+++ src/main/java/com/example/feedprep/domain/matching/controller/TutorMatchingController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,8 @@ import com.example.feedprep.domain.matching.service.TutorMatchingService;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController("/tutor")
+@RestController
+@RequestMapping("/tutor")
 @RequiredArgsConstructor
 public class TutorMatchingController {
 
