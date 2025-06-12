3e49d2e38a9012191056cf9d91f90e38088a4f30
soonch67890 <soonch67890@gmail.com>
2025-06-12 19:52:46 +0900

    fix : 오류 수정

--- src/main/java/com/example/feedprep/domain/user/service/AdminServiceImpl.java
+++ src/main/java/com/example/feedprep/domain/user/service/AdminServiceImpl.java
@@ -14,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService{
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional
