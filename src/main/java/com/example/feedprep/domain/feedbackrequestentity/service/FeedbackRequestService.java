bec90b87ed5095a3e7557273a184e7af9fe155ff
Paragon <45257459+Faragon02@users.noreply.github.com>
2025-06-12 14:29:35 +0900

    feat: 응답 DTO 변경 및 통일

--- src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackRequestService.java
+++ src/main/java/com/example/feedprep/domain/feedbackrequestentity/service/FeedbackRequestService.java
@@ -3,12 +3,11 @@ package com.example.feedprep.domain.feedbackrequestentity.service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.example.feedprep.common.response.ApiResponseDto;
 import com.example.feedprep.domain.feedbackrequestentity.common.RequestState;
 import com.example.feedprep.domain.feedbackrequestentity.dto.request.FeedbackRejectRequestDto;
 import com.example.feedprep.domain.feedbackrequestentity.dto.request.FeedbackRequestDto;
 import com.example.feedprep.domain.feedbackrequestentity.dto.response.FeedbackRequestEntityResponseDto;
-import com.example.feedprep.domain.feedbackrequestentity.dto.response.TutorSideFeedbackRequestDto;
+import com.example.feedprep.domain.feedbackrequestentity.dto.response.FeedbackResponseDetailsDto;
 
 public interface FeedbackRequestService {
 	// 피드백 신청 생성
@@ -25,17 +24,17 @@ public interface FeedbackRequestService {
 		int size
 	);
 	// 피드백 요청 단건 상세 조회(튜터)
-	TutorSideFeedbackRequestDto getFeedbackRequest(Long tutorId, Long requestId);
+	FeedbackResponseDetailsDto getFeedbackRequest(Long tutorId, Long requestId);
 
 	// 피드백 요청 다건 조회 (튜터)
-	List<TutorSideFeedbackRequestDto> getFeedbackRequests(Long tutorId, Integer page, Integer size);
+	List<FeedbackResponseDetailsDto> getFeedbackRequests(Long tutorId, Integer page, Integer size);
 
 	// 피드백 신청 수정
 	FeedbackRequestEntityResponseDto updateRequest(Long userId, Long feedbackRequestId, FeedbackRequestDto dto);
 
 	// 피드백 신청 취소
-	ApiResponseDto cancelRequest(Long userId, Long feedbackRequestId);
+	FeedbackRequestEntityResponseDto cancelRequest(Long userId, Long feedbackRequestId);
 
 	// 피드백 요청 거절(튜터)
-	ApiResponseDto rejectFeedbackRequest(Long tutorId, Long requestId, Integer rejectNumber,  FeedbackRejectRequestDto dto);
+	FeedbackRequestEntityResponseDto  rejectFeedbackRequest(Long tutorId, Long requestId, Integer rejectNumber,  FeedbackRejectRequestDto dto);
 }
