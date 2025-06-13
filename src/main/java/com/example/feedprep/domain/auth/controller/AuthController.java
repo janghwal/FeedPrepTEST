0310a8b3b522200a48cf4ba6f13cb3805504e4e7
YounGyoungMo <gyoungmo2011@naver.com>
2025-06-13 12:48:11 +0900

    feat : 리프레시 토큰을 통한 액세스/리프레시 토큰 재발급 기능 구현

--- src/main/java/com/example/feedprep/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/feedprep/domain/auth/controller/AuthController.java
@@ -1,16 +1,17 @@
 package com.example.feedprep.domain.auth.controller;
+
 import com.example.feedprep.common.exception.enums.SuccessCode;
 import com.example.feedprep.common.response.ApiResponseDto;
 import com.example.feedprep.common.security.annotation.AuthUser;
-import com.example.feedprep.common.security.jwt.CustomUserDetails;
-import com.example.feedprep.common.security.jwt.JwtUtil;
-import com.example.feedprep.domain.auth.dto.*;
+import com.example.feedprep.domain.auth.dto.LoginRequestDto;
+import com.example.feedprep.domain.auth.dto.SignupRequestDto;
+import com.example.feedprep.domain.auth.dto.SignupResponseDto;
+import com.example.feedprep.domain.auth.dto.TokenResponseDto;
 import com.example.feedprep.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -55,6 +56,18 @@ public class AuthController {
         authService.withdraw(authHeader, userId);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ApiResponseDto.success(SuccessCode.WITHDRAW_SUCCESS));
+    }
+
+    // access token 재발급
+    @PostMapping("/refresh")
+    ResponseEntity<ApiResponseDto<TokenResponseDto>> refresh(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestHeader("Refresh") String refreshToken,
+            @AuthUser Long userId
+    ) {
+        TokenResponseDto responseDto =  authService.refresh(authHeader, refreshToken, userId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponseDto.success(SuccessCode.TOKEN_REFRESH_SUCCESS, responseDto));
     }
 
 }
